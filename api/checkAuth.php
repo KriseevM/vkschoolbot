b7f9ac4255cf8f30a6253f837f71d2ab55542480
Kriseev Mikhail (arch) <mkris.yt@gmail.com>
2020-04-10 02:42:24 +0300

    Небольшое исправление методов API Данные для подключения к бд из двух файлов конфликтовали, и неправильное расположение приводило к ошибкам. Исправил это

--- api/checkAuth.php
+++ api/checkAuth.php
@@ -2,8 +2,8 @@
 require 'APIInternalInfo.php';
 $query = "Select expiration_time from $dbkeystable where passkey=\"$key\" and ip=\"$ip\"";
 $time = time();
-$link = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die("false");
-$res = mysqli_query($link, $query);
+$userdblink = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die("false");
+$res = mysqli_query($userdblink, $query);
 if(mysqli_num_rows($res) == 0)
 {
     $auth = false;
@@ -15,14 +15,14 @@ else
     if($time > $exp_time)
     {
         $remove_query = "Delete from $dbkeystable where passkey=\"$key\"";
-        mysqli_query($link, $remove_query);
+        mysqli_query($userdblink, $remove_query);
         $auth = false;
         die("{\"error\":\"Key is expired\"}");
     }
     else
     {
         $reset_time_query = "Update $dbkeystable Set expiration_time=".($time+1800)." where passkey=\"$key\"";
-        mysqli_query($link, $reset_time_query);
+        mysqli_query($userdblink, $reset_time_query);
         $auth = true;
     }
 }
