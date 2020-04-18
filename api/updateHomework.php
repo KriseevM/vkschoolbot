9581c8ff9c64350eabccab84904302abcd7fb89d
Kriseev Mikhail (arch) <mkris.yt@gmail.com>
2020-04-18 18:13:12 +0300

    Обновил updateHomework.php
    В методе updateHomework.php теперь новая проверка входных данных

--- api/updateHomework.php
+++ api/updateHomework.php
@@ -1,29 +1,36 @@
 <?php
-
+require_once '../vendor/autoload.php';
 $input = file_get_contents('php://input');
-if(input != "")
+if($input != "")
 {
 	$data = json_decode($input);
         $ip = $_SERVER['REMOTE_ADDR'];
-        if(!isset($data->key)) 
+        $validator = new JsonSchema\Validator;
+        $schema = '{"type":"object", "properties":'
+                . '{"key":{"type":"string", "required":"true"},'
+                . '"ID":{"type":"integer", "required":"true"},'
+                . '"Homework":{"type":"string", "required":"true"}'
+                . '}'
+            . '}';        
+        $validator->validate($data, json_decode($schema));
+        if(!$validator->isValid())
         {
-            die('{"error":"Key is required for authorisation","errorcode":6}');
+            foreach($validator->getErrors() as $error)
+            {
+                die('{"error":"'.$error['message'].'":7}');
+            }
         }
         $key = $data->key;
         include 'checkAuth.php';
-        if(!isset($data->ID))
-        {
-            die('{"error":"Missing required parameter ID","errorcode":7}');
-        }
-        if(!isset($data->Homework))
-        {
-            die('{"error":"Missing required parameter Homework","errorcode":7}');
-        }
         require_once "../dbconnectinfo.php";
         $link = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die("{\"error\":\"Failed to connect to database.\",\"errorcode\":1}");
 	$q = "UPDATE Homeworkdata SET Homework='".$data -> Homework."' WHERE ID = ".$data ->ID;
 	$res = mysqli_query($link, $q) or die('{"error":"Failed to execute SQL query","errorcode":2}');;
 	echo '{"result":true}';
 	
+}
+else
+{
+    die('{"error":"Empty request","errorcode":7}');
 }
 ?>
