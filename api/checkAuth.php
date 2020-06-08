88ab28f65ba37e4a894bf2b080b89bbf85d0b6a6
Kriseev Mikhail (arch) <mkris.yt@gmail.com>
2020-06-09 02:13:13 +0300

    Проверка ключа в api/CheckAuth.php

--- api/checkAuth.php
+++ api/checkAuth.php
@@ -4,11 +4,15 @@ if(!isset($key) || !isset($ip))
 {
     die('{"error":"Missing required parameters for authorisation","errorcode":6}');
 }
-$query = "Select expiration_time from PassKeys where passkey=\"$key\" and ip=\"$ip\"";
+if(!(preg_match("/^[a-f\d]{64}$/", $key) === 1))
+{
+    die('{"error":"Key has incorrect format":6}');
+}
+$query = "Select expiration_time, user from PassKeys where passkey=\"$key\" and ip=\"$ip\"";
 $time = time();
 $db = new SQLite3("../bot.db");
 $res = $db->query($query)->fetchArray(SQLITE3_NUM);
-if($res == false)
+if($res === false)
 {
     die("{\"error\":\"Key is invalid or your ip address does not match original ip address\",\"errorcode\":4}");
 }
@@ -25,5 +29,7 @@ else
     {
         $reset_time_query = "Update PassKeys Set expiration_time=".($time+1800)." where passkey=\"$key\"";
         $db->exec($reset_time_query) or die('{"error":"Failed to execute SQL query","errorcode":2}');
+        $auth_user=$res[1];
+        $auth_pr = $db->query("Select pr_level from UserData where user=\"$auth_user\";")->fetchArray(SQLITE3_NUM)[0];
     }
 }
