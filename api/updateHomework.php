e89a6f6fa676a580ceab1434b2f2e7e4aae95258
Mikhail <34946346+KriseevM@users.noreply.github.com>
2020-07-05 22:12:09 +0300

    Исправление уязвимости #16 в updateHomework.php
    Исправлена уязвимость для SQL-инъекций в updateHomework.php
    success в выводе заменён на updated

--- api/updateHomework.php
+++ api/updateHomework.php
@@ -6,27 +6,32 @@ if($input != "")
     
         //Checks and auth
 	$data = json_decode($input);
-        $validator = new JsonSchema\Validator;
-        $schema = '{"type":"object", "properties":'
-                . '{"key":{"type":"string", "required":"true"},'
-                . '"ID":{"type":"integer", "required":"true"},'
-                . '"Homework":{"type":"string", "required":"true"}'
-                . '}'
-            . '}';        
-        $validator->validate($data, json_decode($schema));
-        if(!$validator->isValid())
+    $validator = new JsonSchema\Validator;
+    $schema = '{"type":"object", "properties":'
+            . '{"key":{"type":"string", "required":"true"},'
+            . '"ID":{"type":"integer", "required":"true"},'
+            . '"Homework":{"type":"string", "required":"true"}'
+            . '}'
+        . '}';        
+    $validator->validate($data, json_decode($schema));
+    if(!$validator->isValid())
+    {
+        foreach($validator->getErrors() as $error)
         {
-            foreach($validator->getErrors() as $error)
-            {
-                die('{"error":"'.$error['message'].'":7}');
-            }
+            die('{"error":"'.$error['message'].'":7}');
         }
-        $key = $data->key;
-        include 'checkAuth.php';
-        
-        $query = "UPDATE Homeworkdata SET Homework='".$data -> Homework."' WHERE ID = ".$data ->ID;
-	$result = $db->exec($query);
-	echo json_encode(array('success' => $result), JSON_UNESCAPED_UNICODE);
+    }
+    $key = $data->key;
+    include 'checkAuth.php';
+    $homework = $data->Homework;
+    $ID = $data->ID;
+    $query = "UPDATE Homeworkdata SET Homework=:homework WHERE ID = :id";
+    $stmt = $db->prepare($query);
+    $stmt->bindValue(':homework', $homework);
+    $stmt->bindValue(':id', $ID);
+    $stmt->execute();
+    $result = boolval($db->changes());
+	echo json_encode(array('updated' => $result), JSON_UNESCAPED_UNICODE);
 	
 }
 else
