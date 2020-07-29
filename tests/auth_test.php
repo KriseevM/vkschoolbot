412f832e8c44ac7369b3dd6bc07fdaf4c475ea0e
Mikhail <34946346+KriseevM@users.noreply.github.com>
2020-07-30 00:52:19 +0300

    Исправил проблему прав файлов при тестах
    Теперь все тестируемые файлы будут иметь владельца www-data, чтобы сервер имел правильный доступ к базе данных. Также улучшил тест auth_test.php

--- tests/auth_test.php
+++ tests/auth_test.php
@@ -26,28 +26,40 @@ $auth_data = array(
     'pass' => '1234'
 );
 $auth_ch = curl_init('localhost/vkschoolbot/api/auth.php');
-curl_setopt($auth_ch, CURLOPT_POST, 1);
-curl_setopt($auth_ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($auth_ch, CURLOPT_POST, true);
+curl_setopt($auth_ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($auth_ch, CURLOPT_POSTFIELDS, $auth_data);
 $data = json_decode(curl_exec($auth_ch));
-restore_db();
 if($data == null)
 {
-    log_error("Error: incorrect json in response\n");
+    log_error("Error: incorrect response\n");
+    restore_db();
     die();
 }
 else if(isset($data->error))
 {
     log_error("Error ({$data->errorcode}): {$data->error}\n");
+    restore_db();
     die();
 }
 else if(!isset($data->key) || !(preg_match("/^[a-f\d]{64}$/", $data->key)))
 {
     log_error("Error: incorrect key format\n");
+    restore_db();
+    die();
+}
+
+$db = new SQLite3('bot.db');
+$res = $db->query("SELECT * FROM PassKeys WHERE passkey=\"{$data->key}\"")->fetchArray(SQLITE3_NUM);
+if($res === false)
+{
+    log_error("Error: could not add key to database\n");
+    restore_db();
     die();
 }
 else
 {
-    echo "Test passed!\n";
+    restore_db();
+    echo "OK\n";
 }
 
