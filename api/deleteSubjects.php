20aca952c7c0b491d33ba4237a99054c174790f0
Mikhail <34946346+KriseevM@users.noreply.github.com>
2020-07-30 00:58:04 +0300

    Убрал проверку ключа из схемы в deleteSubjects.php

--- api/deleteSubjects.php
+++ api/deleteSubjects.php
@@ -1,5 +1,10 @@
 <?php
 require_once '../vendor/autoload.php';
+include 'checkAuth.php';
+if($auth_pr !== 2)
+{
+    die("{\"error\":\"You are not allowed to use this method\",\"errorcode\":9}");
+}
 $input = file_get_contents('php://input');
 if($input != "")
 {
@@ -7,12 +12,19 @@ if($input != "")
         //Checks and auth
 	$data = json_decode($input);
     $validator = new JsonSchema\Validator;
-    $schema = '{"type":"object", "properties":'
-                . '{"key":{"type":"string", "required":"true"},'
-                . '"IDs":{"type":"array","items":{"type":"integer"}, "required":"true"}'
-                . '}'
-            . '}';        
-    $validator->validate($data, json_decode($schema));
+    $schema = (object)[
+        'type' => 'object',
+        'properties' => (object)[
+            'IDs' => (object)[
+                'type' => 'array',
+                'items' => (object)[
+                    'type' => 'integer'
+                ],
+                'required' => true
+            ]
+        ]
+    ];     
+    $validator->validate($data, $schema);
     if(!$validator->isValid())
     {
         foreach($validator->getErrors() as $error)
@@ -20,16 +32,12 @@ if($input != "")
             die('{"error":"'.$error['message'].'":7}');
         }
     }
-    $key = $data->key;
-    include 'checkAuth.php';
+    
     // Переменная $db приходит из файла checkAuth.php. 
     // Но в этом файле происходит запись в базу, что влияет на вывод метода changes()
     $db->close();
     $db->open("../bot.db");
-    if($auth_pr !== 2)
-    {
-        die("{\"error\":\"You are not allowed to use this method\",\"errorcode\":9}");
-    }
+    
     $placeholders = rtrim(str_repeat('?, ', count($data->IDs)), ', ');
     $query = "DELETE FROM Homeworkdata WHERE ID IN ($placeholders);";
     $stmt = $db->prepare($query);
