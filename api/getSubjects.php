b8ef2d9919721f78ebc92ae61613daa75c960a87
Mikhail <34946346+KriseevM@users.noreply.github.com>
2020-08-01 00:20:16 +0300

    Убрал параметр ключа из getSubjects.php

--- api/getSubjects.php
+++ api/getSubjects.php
@@ -1,9 +1,4 @@
 <?php
-if(!isset($_GET['key'])) 
-{
-    die('{"error":"Key is required for authorisation","errorcode":6}');
-}
-$key = $_GET['key'];
 include 'checkAuth.php';
 $query = "SELECT ID, Subject FROM Homeworkdata";
 $res = $db->query($query);
