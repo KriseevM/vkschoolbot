04907178f4ba9656f08875f7911f998f54a0843d
Mikhail <34946346+KriseevM@users.noreply.github.com>
2020-08-04 21:51:05 +0300

    Добавил обработку ошибок PHP в auth.php

--- api/auth.php
+++ api/auth.php
@@ -1,15 +1,20 @@
 <?php
 $path = realpath(dirname(__FILE__));
 include $path.'/API.php';
-$ip = $_SERVER['REMOTE_ADDR'];
-$user = $_POST['user'];
-$pass = $_POST['pass'];
 try
 {
+
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $user = $_POST['user'];
+    $pass = $_POST['pass'];
     $key = API::auth($user, $pass, $ip);
     echo json_encode(['key' => $key]);
 }
 catch(Exception $e)
 {
     die(json_encode(['error' => $e->getMessage(), 'errorcode' => $e->getCode()]));
+}
+catch(TypeError $e)
+{
+    die(json_encode(['error' => API::ERROR_MISSING_AUTH_DATA, 'errorcode' => 6]));
 }
