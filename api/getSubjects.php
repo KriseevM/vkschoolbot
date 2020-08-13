d2dff7550eb6072f0d8aec4bdbcf217b2b554eb4
Mikhail <34946346+KriseevM@users.noreply.github.com>
2020-08-13 18:26:09 +0300

    Добавил обработку ошибок в getSubjects.php

--- api/getSubjects.php
+++ api/getSubjects.php
@@ -1,7 +1,16 @@
 <?php
-include 'API.php';
-$key = $_SERVER['HTTP_KEY'];
-$ip = $_SERVER['REMOTE_ADDR'];
-$api = new API($key, $ip);
-$result = $api->get_subjects_method();
-echo json_encode($result, JSON_UNESCAPED_UNICODE);
+
+use PHPUnit\Util\Type;
+
+try {
+    include 'API.php';
+    $key = $_SERVER['HTTP_KEY'];
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $api = new API($key, $ip);
+    $result = $api->get_subjects_method();
+    echo json_encode($result, JSON_UNESCAPED_UNICODE);
+} catch (Exception $e) {
+    die(json_encode(['error' => $e->getMessage(), 'errorcode' => $e->getCode()]));
+} catch (TypeError $e) {
+    die(json_encode(['error' => API::ERROR_INVALID_KEY, 'errorcode' => 4]));
+}
